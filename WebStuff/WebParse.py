cda418cfbe236cca81f0a546ef09e07078182440
Michael Wang <milewa@umich.edu>
2016-05-19 12:25:54 -0400

    Fixed the pokemon files, added a makefile

--- WebStuff/WebParse.py
+++ WebStuff/WebParse.py
@@ -10,16 +10,22 @@ allTables = soup.findAll("table", {"align" : "center"});
 gen = 1;
 
 for table in allTables:
-    Gen = "Gen" + str(gen);
+    Gen = "Gen" + str(gen) + ".txt";
     f = open(Gen, 'w')
     row = table.find_all('tr');
 
     for info in row:
         data = info.find_all('td');
+        
+        #takes care of the table with no data in it
         if not data:
             continue
+
         pId = data[1].getText().rstrip().encode("utf8")
+        pId = pId[2:]
+
         name = data[3].find('a').getText().encode("utf8")
+
         type1 = type2 = ''
         if int(data[4]["colspan"]) == 1:
             type1 = data[4].find('a').getText().encode("utf8")
@@ -31,17 +37,3 @@ for table in allTables:
 
     f.close()
     gen += 1
-
-
-
-#for table in allTables:
-#   row = table.find_all('tr');
-#    data = row.find_all('td')
-#    print data[0]
-
-#value = soup.select('tr')
-
-#print value[2];
-
-
-
